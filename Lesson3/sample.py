55d7536f42f4bcfe22e555af28f2ae5e7da8fb83
shashi8877 <53368694+shashi8877@users.noreply.github.com>
2020-01-02 15:06:35 +0530

    Update sample.py

--- Lesson3/sample.py
+++ Lesson3/sample.py
@@ -6,7 +6,7 @@ class Message():
         """
         a simple function that prints a message
         """
-        printf 'Hello World!'
+        print 'Hello World!'
 
 
 if __name__ == "__main__":
